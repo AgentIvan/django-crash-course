443d0742c82dc43a1b9cecd82e42832f18dbd3ef
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-06 20:09:12 -0600

    Add template

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from .models import Todo
+
 
 def todo_list(request):
-    return render(request, "todo_list2.html")
+    todos = Todo.objects.all()
+    context = {"todo_list": todos}
+    return render(request, "todo/todo_list.html", context)
