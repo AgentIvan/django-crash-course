586a8530077ea3db4976cb9544ed70bb5ed99ec2
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-06 19:24:12 -0600

    add templates

--- crashcourse/urls.py
+++ crashcourse/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo.views import todo_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", todo_list),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def todo_list(request):
+    return render(request, "todo_list2.html")
