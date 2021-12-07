306a448b1064888399ee4f9a841ed8047cd714bc
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-06 23:51:53 -0600

    Add todo_detail

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import todo_list
+from .views import todo_detail, todo_list
 
 app_name = "todos"
 
-urlpatterns = [path("", todo_list)]
+urlpatterns = [
+    path("", todo_list),
+    path("<id>/", todo_detail),
+]
--- todo/views.py
+++ todo/views.py
@@ -8,3 +8,9 @@ def todo_list(request):
     todos = Todo.objects.all()
     context = {"todo_list": todos}
     return render(request, "todo/todo_list.html", context)
+
+
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {"todo": todo}
+    return render(request, "todo/todo_detail.html", context)
