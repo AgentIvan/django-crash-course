65ddfc665c3069395d54bd1b678b201fc673c76d
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-06 18:43:06 -0600

    Register model Todo

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Todo(models.Model):
     name = models.CharField(max_length=100)
     due_date = models.DateField()
+
+    def __str__(self):
+        return self.name
